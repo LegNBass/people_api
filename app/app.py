83ef80ce49323ff6714e603d114ec79a0ee1d417
Charlie Armentrout <charmentroutv@gmail.com>
2020-10-27 15:05:37 -0400

    Initial setup of the Flask API and Docker stuff

---
+++ app/app.py
@@ -0,0 +1,17 @@
+import os
+
+from flask import Flask
+
+
+app = Flask(__name__)
+
+@app.route('/hello/<string:name>', methods=['GET'])
+def hello(name):
+    return f'Hello {name}'
+
+
+if __name__ == '__main__':
+    app.run(
+        host='0.0.0.0',
+        debug=os.environ.get('DEBUG', False)
+    )
